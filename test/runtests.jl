1ec6110029c27ef5d821149159226a0b3a8c2277
Johnny Chen <johnnychen94@hotmail.com>
2018-09-04 00:07:19 +0800

    Fix typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ ambs = detect_ambiguities(ImageTransformations, CoordinateTransformations, Base,
 tests = [
     "autorange.jl",
     "resizing.jl",
-    "rotation.jl"
+    "rotation.jl",
     "interpolations.jl",
     "warp.jl",
 ]
