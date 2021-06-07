8c60c639e135de0cac1ee21ca7ae2ee66a3fa2ce
ginkul <geenkool@yandex.ru>
2021-06-07 23:54:46 +0300

    add examples of imresize with fixed point

--- src/resizing.jl
+++ src/resizing.jl
@@ -83,6 +83,10 @@ imresize(img, (1:256, )) # 256*768
 imresize(img, ratio = 0.5) #256*384
 imresize(img, ratio = (2, 1)) # 1024*768
 
+# pass `FixedPoint`
+imresize(img, (256, 384), FixedPoint(10, 15)) # 256*384, img[10,15] == imgr[10,15]
+imresize(img, FixedPoint(10, 15), ratio = 0.5) # 256*384, img[10,15] == imgr[10,15]
+
 # use different interpolation method
 imresize(img, (256, 384), method=Linear()) # 256*384 bilinear interpolation
 imresize(img, (256, 384), method=Lanczos4OpenCV()) # 256*384 OpenCV-compatible Lanczos 4 interpolation
