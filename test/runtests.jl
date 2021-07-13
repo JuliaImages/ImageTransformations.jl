836bdda2b15bad16e5931a23b2423d4332fa2ca6
Johnny Chen <johnnychen94@hotmail.com>
2021-07-13 16:23:23 +0800

    remove all summary test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,6 @@ using ImageTransformations
 ambs = detect_ambiguities(ImageTransformations, CoordinateTransformations, Base, Core)
 @test isempty(setdiff(ambs, refambs))
 
-function typestring(::Type{T}) where T   # from https://github.com/JuliaImages/ImageCore.jl/pull/133
-    buf = IOBuffer()
-    show(buf, T)
-    String(take!(buf))
-end
-
 # helper function to compare NaN
 nearlysame(x, y) = x ≈ y || (isnan(x) & isnan(y))
 nearlysame(A::AbstractArray, B::AbstractArray) = all(map(nearlysame, A, B))
