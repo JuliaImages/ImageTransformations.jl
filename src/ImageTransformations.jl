dc82fd9fa18f53776caa840b20d2fdea53fd1618
Zelun Wang <zelunwang@gmail.com>
2021-11-02 15:13:46 -0500

    Reorder `WarpedView` and `warp` docstring; clarify `restrict` docstring

--- src/ImageTransformations.jl
+++ src/ImageTransformations.jl
@@ -2,12 +2,12 @@
 
 This package provides support for image resizing, image rotation, and other spatial transformations of arrays.
 
-- `InvWarpedView`: Create a view of `img` that lazily transforms any given index `I` passed to `wv[I]` so that `wv[I] == img[inv(tinv)(I)]`.
 - `WarpedView`: Create a view of `img` that lazily transforms any given index `I` passed to `wv[I]` so that `wv[I] == img[tform(I)]`.
+- `warp`: An eager evaluation variant of `WarpedView`. Transform the coordinates of `img`, returning a new `imgw` satisfying `imgw[I] = img[tform(I)]`. 
+- `InvWarpedView`: Create a view of `img` that lazily transforms any given index `I` passed to `wv[I]` so that `wv[I] == img[inv(tinv)(I)]`.
 - `imresize`: Upsample/downsample the image `img` to a given size `sz` or axes `inds` using interpolations.
-- `restrict`: A much more efficient version of `imresize` that two-folds/down-samples image to approximate 1/2 size. (This is now provided by ImageBase.)
+- `restrict`: A much more efficient version of `imresize` that two-folds/down-samples image to approximate 1/2 size. (This is now provided by `ImageBase.restrict`)
 - `imrotate`: Rotate image `img` by `θ`∈[0,2π) in a clockwise direction around its center point.
-- `warp`: Transform the coordinates of `img`, returning a new `imgw` satisfying `imgw[I] = img[tform(I)]`.
 - `autorange`: For given transformation `tform`, return the "smallest" range indices that preserves all information from `A` after applying `tform`.
 
 There are in-place version of many of the functions, e.g., imresize! etc.
