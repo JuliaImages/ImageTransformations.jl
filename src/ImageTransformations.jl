4ce509c3faa754b60fa89a461bb28cb7ee78a66e
zelunw <92692040+zelunw@users.noreply.github.com>
2021-11-05 03:57:43 -0500

    Add an overall module docstring (#152)
    Co-authored-by: Johnny Chen <johnnychen94@hotmail.com>
    Co-authored-by: Tim Holy <tim.holy@gmail.com>

--- src/ImageTransformations.jl
+++ src/ImageTransformations.jl
@@ -1,3 +1,40 @@
+"""
+
+This package provides support for image resizing, image rotation, and other spatial transformations of arrays.
+
+- `WarpedView`: Create a view of `img` that lazily transforms any given index `I` passed to `wv[I]` so that `wv[I] == img[tform(I)]`.
+- `warp`: An eager evaluation variant of `WarpedView`. Transform the coordinates of `img`, returning a new `imgw` satisfying `imgw[I] = img[tform(I)]`. 
+- `InvWarpedView`: Create a view of `img` that lazily transforms any given index `I` passed to `wv[I]` so that `wv[I] == img[inv(tinv)(I)]`.
+- `imresize`: Upsample/downsample the image `img` to a given size `sz` or axes `inds` using interpolations.
+- `restrict`: A much more efficient version of `imresize` that two-folds/down-samples image to approximate 1/2 size. (This is now provided by `ImageBase.restrict`)
+- `imrotate`: Rotate image `img` by `θ`∈[0,2π) in a clockwise direction around its center point.
+- `autorange`: For given transformation `tform`, return the "smallest" range indices that preserves all information from `A` after applying `tform`.
+
+There are in-place version of many of the functions, e.g., `imresize!` etc.
+
+Resize example:
+
+```jldoctest
+using ImageTransformations, TestImages
+
+img = testimage("mandrill")
+
+img_small = imresize(img, ratio=1/8)
+img_medium = imresize(img_small, size(img_small).*2)
+```
+
+Warping example:
+
+```jldoctest
+using ImageTransformations, TestImages, CoordinateTransformations, Rotations
+
+img = testimage("camera");
+
+# define transformation
+trfm = recenter(RotMatrix(pi/8), center(img));
+imgw = warp(img, trfm);
+```
+"""
 module ImageTransformations
 
 using ImageCore
