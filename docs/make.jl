edccf2f5b08baf25baaf8c162b0d5c3676118902
Ian Weaver <weaveric@gmail.com>
2026-06-24 00:50:39 -0700

    Update to Documenter.jl v1 (#176)

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using Documenter, DemoCards
+using Documenter.Remotes: GitHub
 using ImageBase, ImageTransformations, ImageShow
 using TestImages
 using CoordinateTransformations, Interpolations, Rotations
@@ -9,15 +10,17 @@ testimage.(["cameraman", "lighthouse"])
 examples, postprocess_cb, examples_assets = makedemos("examples")
 format = Documenter.HTML(edit_link = "master",
                          prettyurls = get(ENV, "CI", nothing) == "true",
-                         assets = [examples_assets,])
+                         assets = [examples_assets,], size_threshold = nothing)
 
 makedocs(modules  = [ImageTransformations, ImageBase],
+         repo     = GitHub("JuliaImages/ImageTransformations.jl"),
          format   = format,
          sitename = "ImageTransformations",
          pages    = [
              "index.md",
              examples,
-             "reference.md"])
+             "reference.md"],
+         warnonly = [:missing_docs])
 
 postprocess_cb()
 
